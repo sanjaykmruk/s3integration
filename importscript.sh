01e8102ace32f1b9e84c2bf5f6f585d8c059ee13
sanjay kumar <53774623+sanjaykmruk@users.noreply.github.com>
2024-04-23 16:13:15 +0100

    Update importscript.sh

--- importscript.sh
+++ importscript.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-cd $1
+cd ./
 pwd
-n=10
+n=3
 fileToMove=""
 ls -1 *.json | sed 's/.json$//' | while read col; do
    echo  $col;
-   mongoimport --uri "mongodb+srv://temp_michael:am2SWrMoMX9I8vZp@dev-pluto-azure-eun-001.bgj1m.mongodb.net/dev-lsauthmigration-002"  --collection "lsauth_test_vm_multi_5" --file $col.json  --type json&
-     fileToMove= "${col},${fileToMove}"
-  (( ++count % n == 0 )) && wait
-  mv -R ${fileToMove} ./dirToMove/	
+   fileToMove="$col.json $fileToMove";
+ 
+   if ((  ++count % $n == 0 )) ; then
+         wait
+         mv -f $fileToMove ./dirToMove/
+   fi
+ 
 done
